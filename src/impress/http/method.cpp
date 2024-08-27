ab33ca0d9e5781621513a6f356bfb009ebb04b45
Tal Alon <talalon369@gmail.com>
2024-08-27 03:57:59 +0300

    bump c++ standard to 20 and fix get_method impl

--- src/impress/http/method.cpp
+++ src/impress/http/method.cpp
@@ -4,5 +4,8 @@ using namespace std;
 using namespace impress;
 
 optional<Method> impress::get_method(const string &method_str) {
-    return HTTP_METHODS.at(method_str); // TODO check if method_str exists in HTTP_METHODS
+    if (HTTP_METHODS.contains(method_str)) {
+        return HTTP_METHODS.at(method_str);
+    }
+    return nullopt;
 }
