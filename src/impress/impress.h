89459985825c6462093ea8362f56398e30f6e40b
Tal Alon <talalon369@gmail.com>
2024-08-28 22:35:43 +0300

    added missing includes

--- src/impress/impress.h
+++ src/impress/impress.h
@@ -1,10 +1,16 @@
 #pragma once
+#include "impress/http/headers.h"
 #include "impress/http/method.h"
+#include "impress/http/path.h"
 #include "impress/http/request.h"
+#include "impress/http/response.h"
 #include "impress/http/status.h"
+
 #include "impress/logging/logger.h"
 #include "impress/logging/stream_logger.h"
-#include "impress/tcp/server.h"
-#include "impress/tcp/connection.h"
+
+#include "impress/server/connection.h"
+#include "impress/server/router.h"
+#include "impress/server/server.h"
 
 #define IMPRESS_VERSION "0.1.0"
