8b10dd44b56aebec97ec082ac7f8790963b37d86
Tal Alon <talalon369@gmail.com>
2024-08-29 23:26:02 +0300

    fixed include paths

--- src/impress/impress.h
+++ src/impress/impress.h
@@ -1,16 +1,16 @@
 #pragma once
-#include "impress/http/headers.h"
-#include "impress/http/method.h"
-#include "impress/http/path.h"
-#include "impress/http/request.h"
-#include "impress/http/response.h"
-#include "impress/http/status.h"
+#include "http/headers.h"
+#include "http/method.h"
+#include "http/path.h"
+#include "http/request.h"
+#include "http/response.h"
+#include "http/status.h"
 
-#include "impress/logging/logger.h"
-#include "impress/logging/stream_logger.h"
+#include "logging/logger.h"
+#include "logging/stream_logger.h"
 
-#include "impress/server/connection.h"
-#include "impress/server/router.h"
-#include "impress/server/server.h"
+#include "server/connection.h"
+#include "server/router.h"
+#include "server/server.h"
 
 #define IMPRESS_VERSION "0.1.0"
